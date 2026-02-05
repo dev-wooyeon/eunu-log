0df6352eaa13dae5721f97aca2c2f5b433c331ac
ieunune <une@kakao.com>
2026-02-06 00:02:07 +0900

    fix : RSS XML 네임스페이스 수정

--- src/app/feed.xml/route.ts
+++ src/app/feed.xml/route.ts
@@ -23,7 +23,7 @@ export async function GET() {
         .join('');
 
     const rss = `<?xml version="1.0" encoding="UTF-8" ?>
-<rss version="2.0" xmlns:atom="http://www.w3.org/2000/svg">
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
   <channel>
     <title>eunu.log</title>
     <link>${SITE_URL}</link>
